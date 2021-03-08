9110f9fc94581be98837dc5360b06ea8e222cf0e
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-03-08 10:17:04 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <h3>{movie.title}</h3>
+            <p>Run Time: {movie.time} minutes</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
